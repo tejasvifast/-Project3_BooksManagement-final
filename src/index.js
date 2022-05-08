517b52efb04189b92e2f5b726b6cb3b0876fde91
tejasvi.fast <tejasvi.fast@gmail.com>
2022-05-09 00:43:52 +0530

    first

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-Parser");
+const mongoose =require("mongoose")
+const router = require("./routes/route");
+const { route } = require("../../project2-internRequirement-final/src/routes/route");
+
+const app = express();
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+mongoose.connect("mongodb+srv://Functionup_user1:xjCaggwJRDuZ6bjP@my-first-cluster.sfq4n.mongodb.net/Project3",{useNewUrlParser:true})
+.then( () => console.log("mongoDb is Connected"))
+.catch( err => console.log(err.message))
+
+app.use("/",route);
+
+app.listen(process.env.PORT||3000 , function (){
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
